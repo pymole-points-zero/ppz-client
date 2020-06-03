212cddf836476b18e621745a177ae74019114bdf
pymole <goglbummm@gmail.com>
2020-06-03 19:17:26 +0500

    windows and linux virtualenv paths

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
 from pathlib import Path
+import os
+
 
 BASE_PATH = Path.cwd()
 CREDENTIALS_PATH = Path('credentials.json')
 ENGINE_MAIN_PATH = BASE_PATH.parent / 'ppz-engine' / 'main.py'
-VENV_PATH = BASE_PATH.parent / 'ppz-engine' / 'venv' / 'bin' / 'python'
+
+if os.name == 'nt':
+    VENV_PATH = BASE_PATH.parent / 'ppz-engine' / 'venv' / 'Scripts' / 'python'
+elif os.name == 'posix':
+    VENV_PATH = BASE_PATH.parent / 'ppz-engine' / 'venv' / 'bin' / 'python'
+else:
+    raise OSError
+
 NETWORKS_FOLDER = BASE_PATH / 'networks'
-SERVER_URL = 'http://127.0.0.1:8000'
+SERVER_URL = 'http://192.168.0.7:8000'
