1e824871c3baf522af70866544ef793c39cb695e
pymole <goglbummm@gmail.com>
2020-06-03 22:05:58 +0500

    run engine with global interpreter if its virtual environment is not exists

--- config.py
+++ config.py
@@ -6,12 +6,19 @@ BASE_PATH = Path.cwd()
 CREDENTIALS_PATH = Path('credentials.json')
 ENGINE_MAIN_PATH = BASE_PATH.parent / 'ppz-engine' / 'main.py'
 
-if os.name == 'nt':
-    VENV_PATH = BASE_PATH.parent / 'ppz-engine' / 'venv' / 'Scripts' / 'python'
-elif os.name == 'posix':
-    VENV_PATH = BASE_PATH.parent / 'ppz-engine' / 'venv' / 'bin' / 'python'
+# check if virtual environment exists
+# if not then try to run global interpreter
+VENV_PATH = BASE_PATH.parent / 'ppz-engine' / 'venv'
+
+if VENV_PATH.exists():
+    if os.name == 'nt':
+        VENV_PATH = VENV_PATH / 'Scripts' / 'python'
+    elif os.name == 'posix':
+        VENV_PATH = VENV_PATH / 'bin' / 'python'
+    else:
+        raise OSError
 else:
-    raise OSError
+    VENV_PATH = 'python3'
 
 NETWORKS_FOLDER = BASE_PATH / 'networks'
 SERVER_URL = 'http://192.168.0.7:8000'
